5c6463f49b021c13d1f2cf3a8443c2a68b384c82
landstring <landstring@gmail.com>
2024-02-12 20:45:18 +0400

    Исправлена файловая система проекта

--- Econom_main/src/main/java/com/example/econom_main/EconomMainApplication.java
+++ Econom_main/src/main/java/com/example/econom_main/EconomMainApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EconomMainApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EconomMainApplication.class, args);
     }
