5e3df251034e5378b1a0f1465a98950dda5deae0
Yaroslav Buykevich <landstring@gmail.com>
2024-03-30 12:39:46 +0400

    Первая попытка соединения front с back

--- Econom_main/src/main/java/com/example/econom_main/Product/controllers/ProductController.java
+++ Econom_main/src/main/java/com/example/econom_main/Product/controllers/ProductController.java
@@ -3,28 +3,19 @@ package com.example.econom_main.Product.controllers;
 import com.example.econom_main.Product.services.ProductService;
 import com.example.econom_main.Product.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
     @GetMapping("/products")
-    private List<Product> getAllProducts(){
-        return productService.getAllProducts();
-    }
-
-    @GetMapping("/products/{product_id}")
-    private Product getProduct(@PathVariable("product_id") long product_id){
-        return productService.getProductById(product_id);
-    }
-
-    @PostMapping("/add")
-    private void addNewProduct(@RequestBody Product product){
-        productService.addNewProduct(product);
+    private String getProduct(){
+        return "productPage";
     }
 }
