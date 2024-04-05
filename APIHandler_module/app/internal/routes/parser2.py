3317a2fa9ba8bc59628f9b24eb921f9e43dca477
MRossa <maxim.rossa@mail.ru>
2024-04-05 21:21:46 +0400

    Добавлен маршрут для парсера. НЕ ЗАКОНЧЕН

--- APIHandler_module/app/internal/routes/parser2.py
+++ APIHandler_module/app/internal/routes/parser2.py
@@ -4,6 +4,7 @@ router = APIRouter(
     prefix='/api/v2/parser'
 )
 
-@router.post('/items/get_price/')
-def get_price(url: str) -> float:
+@router.post('/item/url/')
+def get_item_data(url: str) -> dict:
     ...
+    return {}
