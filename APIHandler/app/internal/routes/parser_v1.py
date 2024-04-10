e436f309952cb50a12091308399bd793bc2b4422
MRossa <maxim.rossa@mail.ru>
2024-04-10 16:08:22 +0400

    Обновил API Handler
    Добавил поддержку Парсера вер. 2
    Отформатировал контроллеры к общему стандарту (обработка ошибок)

--- APIHandler/app/internal/routes/parser_v1.py
+++ APIHandler/app/internal/routes/parser_v1.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from APIHandler.app.pkg.sbermarket_module.app.sber import ItemParser
 
@@ -8,4 +8,11 @@ router = APIRouter(prefix="/api/v1/parser")
 
 @router.post("/item/url/")
 def get_item_data(url: str) -> dict:
-    return ItemParser.run_item(url)
+    try:
+        item_data = ItemParser.run_item(url)
+        if item_data:
+            return item_data
+        else:
+            raise HTTPException(status_code=404, detail="Item data not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- APIHandler/app/internal/routes/parser_v2.py
+++ APIHandler/app/internal/routes/parser_v2.py
@@ -1,14 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
+from APIHandler.app.pkg.sbermarket2_module.app.src.sber_parser2 import SberParser2
+from APIHandler.app.pkg.sbermarket2_module.app.utils.constants import CHROME_PATH
+from APIHandler.app.pkg.sbermarket2_module.app.utils.data_fetcher import DataFetcher
 
 
 router = APIRouter(prefix="/api/v2/parser")
 
 
 @router.post("/item/url/")
-def get_item_data(url: str) -> dict:
-    # TODO: Доделать V2
-    return {"error": "NotImplementedError"}
-    # parser = itemParser()
-    # item_data = parser.run_item(url)[0]
-
-    # return {"price": float(item_data["price"])}
+def get_item_data(url: str, number_market: int) -> dict:
+    fetcher = DataFetcher(CHROME_PATH)
+    parser = SberParser2(fetcher=fetcher)
+    try:
+        item_data = parser.get_item_data(url, number_market)
+        if item_data:
+            return item_data
+        else:
+            raise HTTPException(status_code=404, detail="Item data not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
