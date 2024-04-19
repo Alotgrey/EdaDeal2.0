57da621b88753760a390e4d0de72a989671e8076
Alexandr Tultsov <100188102+tulec@users.noreply.github.com>
2024-04-19 23:07:52 +0400

    Минорное обновление каталога
    Переработана логика каталога

--- Pattern/js/category-list.js
+++ Pattern/js/category-list.js
@@ -5,22 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     categoryListItems.forEach(item => {
         item.addEventListener('mouseover', function() {
             const subcategories = JSON.parse(this.getAttribute('data-subcategories'));
-            subcategoryList.innerHTML = ''; // Очищаем список подкатегорий перед добавлением новых
+            subcategoryList.innerHTML = ''; 
             subcategories.forEach(subcategory => {
-                // Создаем элемент <a> вместо <li>
+
                 const link = document.createElement('a');
-                link.href = '#'; // Устанавливаем href, если это необходимо
+                link.href = '#'; 
                 link.textContent = subcategory;
-                link.style.textDecoration = 'none'; // Устанавливаем стили, как указано в CSS
+                link.style.textDecoration = 'none';
                 link.style.width = 'auto';
                 link.style.fontSize = '30px';
                 link.style.color = 'black';
                 link.style.transition = 'color 0.3s ease-in-out';
 
-                // Создаем элемент <li> для обертывания ссылки
+
                 const listItem = document.createElement('li');
-                listItem.appendChild(link); // Добавляем ссылку в элемент <li>
-                subcategoryList.appendChild(listItem); // Добавляем элемент <li> в список подкатегорий
+                listItem.appendChild(link); 
+                subcategoryList.appendChild(listItem); 
             });
         });
     });
--- Pattern/js/open-category.js
+++ Pattern/js/open-category.js
@@ -11,7 +11,41 @@ window.addEventListener('keydown', (e) => {
 document.querySelector("#category-popup-id .category-box").addEventListener('click', event => {
     event._isClickWithInModal = true;
 });
+
+
 document.getElementById("category-popup-id").addEventListener('click', event => {
     if (event._isClickWithInModal) return;
     event.currentTarget.classList.remove('open');
+});
+
+
+function toggleCategoryVisibility(categoryId) {
+    const categories = {
+        meat: document.querySelectorAll('.meat-category'),
+        milk: document.querySelectorAll('.milk-category'),
+        sweets: document.querySelectorAll('.sweets-category')
+    };
+
+    for (const categoryType in categories) {
+        if (categoryType !== categoryId) {
+            categories[categoryType].forEach(function(category) {
+                category.classList.remove('open');
+            });
+        }
+    }
+
+    categories[categoryId].forEach(function(category) {
+        category.classList.toggle('open');
+    });
+}
+document.getElementById('milk-category').addEventListener('mouseover', function() {
+    toggleCategoryVisibility('milk');
+});
+
+document.getElementById('meat-category').addEventListener('mouseover', function() {
+    toggleCategoryVisibility('meat');
+});
+
+document.getElementById('sweets-category').addEventListener('mouseover', function() {
+    toggleCategoryVisibility('sweets');
 });
